f99cb5961227c1e5da25159bc2ed31e674a2e5aa
Andreas Henningson <henningsonandreas@gmail.com>
2020-09-22 13:44:17 +0200

    Refactor Navigation

--- src/components/header/NavBar.jsx
+++ src/components/header/NavBar.jsx
@@ -1,23 +1,29 @@
-import React from "react";
-import styled from "styled-components";
-import NavBarLink from "./NavBarLink";
+import React from 'react';
+import styled from 'styled-components';
+import UserKit from '../../data/UserKit';
+import NavBarLink from './NavBarLink';
 
 const UlStyled = styled.ul`
   display: flex;
   justify-content: space-between;
-  width: 150px;
 `;
 
 export default function NavBar() {
+  const userKit = new UserKit();
+  const token = userKit.getToken();
   return (
     <nav>
       <UlStyled>
-        <li>
-          <NavBarLink url="/login" title="Login" />
-        </li>
-        <li>
-          <NavBarLink url="/" title="Home" />
-        </li>
+        {!token && (
+          <li>
+            <NavBarLink url="/login" title="Login" />
+          </li>
+        )}
+        {token && (
+          <li>
+            <NavBarLink url="/home" title="Home" />
+          </li>
+        )}
       </UlStyled>
     </nav>
   );
--- src/components/header/SimpleLayout.jsx
+++ src/components/header/SimpleLayout.jsx
@@ -1,7 +1,7 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import styled from "styled-components";
-import NavBar from "./NavBar";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import NavBar from './NavBar';
 
 const HeaderStyled = styled.header`
   position: fixed;
@@ -19,9 +19,21 @@ const HeaderWrapper = styled.div`
   align-items: center;
 `;
 
-const Title = styled.h1`
-  font-size: 2rem;
-  color: white;
+const Title = styled.div`
+  height: 100%;
+  background: ;
+  a {
+    display: flex;
+    font-size: 2rem;
+    color: white;
+    align-items: center;
+    text-decoration: none;
+    padding: 0.3rem 0.4rem;
+    &:hover {
+      background: #5981b5;
+      transition: background-color 0.2s;
+    }
+  }
 `;
 
 export default function SimpleLayout({ children }) {
@@ -29,7 +41,9 @@ export default function SimpleLayout({ children }) {
     <div>
       <HeaderStyled>
         <HeaderWrapper>
-          <Title>Hej</Title>
+          <Title>
+            <Link to="/">Business</Link>
+          </Title>
           <NavBar />
         </HeaderWrapper>
       </HeaderStyled>
