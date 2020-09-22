72eea215d5ae1cf49e33fdb9dafd6ed270fdc0e6
Andreas Henningson <henningsonandreas@gmail.com>
2020-09-22 12:20:31 +0200

    Add required to renderInput class

--- src/data/BuildKit.js
+++ src/data/BuildKit.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React from 'react';
 
 export default class {
   renderInput(index, placeholder, stateVariable, setStateVariable) {
     return (
       <div key={index}>
         <label>{placeholder}</label>
-        <input placeholder={placeholder} value={stateVariable} onChange={(e) => setStateVariable(e.target.value)} />
+        <input
+          placeholder={placeholder}
+          value={stateVariable}
+          onChange={(e) => setStateVariable(e.target.value)}
+          required
+        />
       </div>
     );
   }
