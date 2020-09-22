6c845a86f1cb2a92fe127dce29fd5f329fbdbf2a
Andreas Henningson <henningsonandreas@gmail.com>
2020-09-22 09:56:03 +0200

    Hotfix

--- src/components/create_customer_page/CreateCustomerForm.jsx
+++ src/components/create_customer_page/CreateCustomerForm.jsx
@@ -12,10 +12,9 @@ export default function CreateCustomerForm() {
   const [email, setEmail] = useState();
   const [phoneNumber, setPhoneNumber] = useState();
 
-  const userKit = new UserKit(),
-  const buildKit = new BuildKit(),
+  const userKit = new UserKit();
+  const buildKit = new BuildKit();
 
-  
   return (
     <form>
       <h1>Hej</h1>
