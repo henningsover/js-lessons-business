39ff8413a586a05f76b8c841ca355076dc6df13e
Andreas Henningson <henningsonandreas@gmail.com>
2020-09-23 14:30:06 +0200

    *Import user context, *Load customer list when delete is done

--- src/components/customer_details_page/DeleteButton.jsx
+++ src/components/customer_details_page/DeleteButton.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useHistory } from 'react-router-dom';
+import { UserContext } from '../../contexts/UserContext';
 import UserKit from '../../data/UserKit';
 
 export default function DeleteButton({ customerId }) {
+  const { setShouldLoadCustomerList } = useContext(UserContext);
   const userKit = new UserKit();
   const history = useHistory();
 
   function handleDelete() {
-    userKit.deleteCustomer(customerId).then(history.push('/home'));
+    userKit.deleteCustomer(customerId).then((res) => {
+      if (res.ok) {
+        setShouldLoadCustomerList(true);
+        history.push('/home');
+      }
+    });
   }
   return <button onClick={handleDelete}>Delete customer</button>;
 }
